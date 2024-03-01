73fec55af98987809ae55fd3019cc1361636a326
Malcolm Still <malcolm.still@gmail.com>
2024-03-01 15:18:28 +0000

    Attributes

--- src/main.zig
+++ src/main.zig
@@ -26,11 +26,11 @@ pub fn main() !void {
     std.debug.print("{any}", .{nodes});
 }
 
-fn processNode(allocator: std.mem.Allocator, node_list: *NodeList, node: ?*xml2.xmlNode) !void {
-    var maybe_cur_node: ?*xml2.xmlNode = node;
+fn processNode(allocator: std.mem.Allocator, node_list: *NodeList, maybe_parent_node: ?*xml2.xmlNode) !void {
+    var maybe_cur_node: ?*xml2.xmlNode = maybe_parent_node;
 
-    while (maybe_cur_node) |n| {
-        const cur_node: *xml2.xmlNode = n;
+    while (maybe_cur_node) |node| {
+        const cur_node: *xml2.xmlNode = node;
         defer maybe_cur_node = cur_node.*.next;
 
         if (cur_node.type != xml2.XML_ELEMENT_NODE) continue;
@@ -38,17 +38,17 @@ fn processNode(allocator: std.mem.Allocator, node_list: *NodeList, node: ?*xml2.
         const tag = std.mem.span(cur_node.name);
 
         if (std.mem.eql(u8, tag, "protocol")) {
-            try node_list.append(.{ .protocol_begin = .{ .name = "" } });
+            try node_list.append(.{ .protocol_begin = .{} });
         } else if (std.mem.eql(u8, tag, "copyright")) {
             try node_list.append(.{ .copyright_begin = .{} });
         } else if (std.mem.eql(u8, tag, "interface")) {
-            try node_list.append(.{ .interface_begin = .{ .name = "" } });
+            try node_list.append(.{ .interface_begin = .{} });
         } else if (std.mem.eql(u8, tag, "description")) {
-            try node_list.append(.{ .description_begin = .{ .summary = "" } });
+            try node_list.append(.{ .description_begin = .{} });
         } else if (std.mem.eql(u8, tag, "event")) {
-            try node_list.append(.{ .event_begin = .{ .name = "" } });
+            try node_list.append(.{ .event_begin = .{} });
         } else if (std.mem.eql(u8, tag, "request")) {
-            try node_list.append(.{ .request_begin = .{ .name = "" } });
+            try node_list.append(.{ .request_begin = .{} });
         } else if (std.mem.eql(u8, tag, "arg")) {
             try node_list.append(.{ .arg_begin = .{} });
         } else if (std.mem.eql(u8, tag, "enum")) {
@@ -59,13 +59,32 @@ fn processNode(allocator: std.mem.Allocator, node_list: *NodeList, node: ?*xml2.
             std.debug.panic("Unexpected tag: {s}", .{tag});
         }
 
-        // var attr = cur_node.*.properties;
-        // while (attr != null) {
-        //     const attrName = std.mem.span(attr.name);
-        //     const attrValue = std.mem.span(attr.children.content);
-        //     std.debug.print("  Attribute: {s}={s}\n", .{ attrName, attrValue });
-        //     attr = attr.*.next;
-        // }
+        std.debug.assert(node_list.nodes.items.len > 0);
+
+        const latest_node = &node_list.nodes.items[node_list.nodes.items.len - 1];
+
+        var attr = cur_node.*.properties;
+        while (attr != null) {
+            const attr_name = std.mem.span(attr.*.name);
+            const attr_value = std.mem.span(attr.*.children.*.content);
+            // std.debug.print("  Attribute: {s}={s}\n", .{ attrName, attrValue });
+
+            const arena_attr_value = try allocator.alloc(u8, attr_value.len);
+            std.mem.copyForwards(u8, arena_attr_value, attr_value);
+
+            switch (latest_node.*) {
+                inline else => |*n| inline for (std.meta.fields(@TypeOf(n.*))) |field| {
+                    if (std.mem.eql(u8, field.name, attr_name)) {
+                        switch (@typeInfo(field.type)) {
+                            .Int => @field(n, field.name) = try std.fmt.parseInt(u32, arena_attr_value, 10),
+                            else => @field(n, field.name) = arena_attr_value,
+                        }
+                    }
+                },
+            }
+
+            attr = attr.*.next;
+        }
 
         try processNode(allocator, node_list, cur_node.children);
 
@@ -192,16 +211,20 @@ const Node = union(NodeEnum) {
         version: u32 = 0,
     },
     interface_end: struct {},
-    description_begin: struct { summary: []const u8 },
+    description_begin: struct { summary: []const u8 = "" },
     description_end: struct {},
-    event_begin: struct { name: []const u8 },
+    event_begin: struct { name: []const u8 = "" },
     event_end: struct {},
-    request_begin: struct { name: []const u8 },
+    request_begin: struct { name: []const u8 = "" },
     request_end: struct {},
     enum_begin: struct {},
     enum_end: struct {},
-    arg_begin: struct {},
-    entry_begin: struct {},
+    arg_begin: struct {
+        name: []const u8 = "",
+        type: []const u8 = "",
+        summary: []const u8 = "",
+    },
+    entry_begin: struct { name: []const u8 = "" },
     arg_end: struct {},
     entry_end: struct {},
 
@@ -221,105 +244,10 @@ const Node = union(NodeEnum) {
             .request_end => |_| try writer.print("</request>", .{}),
             .enum_begin => |_| try writer.print("<enum>", .{}),
             .enum_end => |_| try writer.print("</enum>", .{}),
-            .arg_begin => |_| try writer.print("<arg>", .{}),
+            .arg_begin => |n| try writer.print("<arg name=\"{s}\" type=\"{s}\" summary=\"{s}\">", .{ n.name, n.type, n.summary }),
             .arg_end => |_| try writer.print("</arg>", .{}),
-            .entry_begin => |_| try writer.print("<entry>", .{}),
+            .entry_begin => |n| try writer.print("<entry name=\"{s}\">", .{n.name}),
             .entry_end => |_| try writer.print("</entry>", .{}),
         }
     }
 };
-
-fn oldProcessNode(allocator: std.mem.Allocator, nodes: *NodeList, reader: *xml2.xmlTextReader) !void {
-    const tag_ptr = xml2.xmlTextReaderConstName(reader);
-    const tag = if (tag_ptr) |ptr| std.mem.span(ptr) else "";
-
-    const node_type = xml2.xmlTextReaderNodeType(reader);
-
-    if (node_type == xml2.XML_READER_TYPE_ELEMENT) {
-        if (std.mem.eql(u8, tag, "protocol")) {
-            try nodes.append(.{ .protocol_begin = .{ .name = "" } });
-        } else if (std.mem.eql(u8, tag, "copyright")) {
-            try nodes.append(.{ .copyright_begin = .{} });
-        } else if (std.mem.eql(u8, tag, "interface")) {
-            try nodes.append(.{ .interface_begin = .{ .name = "" } });
-        } else if (std.mem.eql(u8, tag, "description")) {
-            try nodes.append(.{ .description_begin = .{ .summary = "" } });
-        } else if (std.mem.eql(u8, tag, "event")) {
-            try nodes.append(.{ .event_begin = .{ .name = "" } });
-        } else if (std.mem.eql(u8, tag, "request")) {
-            try nodes.append(.{ .request_begin = .{ .name = "" } });
-        } else if (std.mem.eql(u8, tag, "arg")) {
-            try nodes.append(.{ .arg = .{} });
-        } else if (std.mem.eql(u8, tag, "enum")) {
-            try nodes.append(.{ .enum_begin = .{} });
-        } else if (std.mem.eql(u8, tag, "entry")) {
-            try nodes.append(.{ .entry = .{} });
-        } else {
-            std.debug.panic("Unexpected tag: {s}", .{tag});
-        }
-    }
-    if (node_type == xml2.XML_READER_TYPE_END_ELEMENT) {
-        if (std.mem.eql(u8, tag, "protocol")) {
-            try nodes.append(.{ .protocol_end = .{} });
-        } else if (std.mem.eql(u8, tag, "copyright")) {
-            try nodes.append(.{ .copyright_end = .{} });
-        } else if (std.mem.eql(u8, tag, "interface")) {
-            try nodes.append(.{ .interface_end = .{} });
-        } else if (std.mem.eql(u8, tag, "description")) {
-            try nodes.append(.{ .description_end = .{} });
-        } else if (std.mem.eql(u8, tag, "event")) {
-            try nodes.append(.{ .event_end = .{} });
-        } else if (std.mem.eql(u8, tag, "request")) {
-            try nodes.append(.{ .request_end = .{} });
-        } else if (std.mem.eql(u8, tag, "enum")) {
-            try nodes.append(.{ .enum_end = .{} });
-        } else {
-            std.debug.panic("Unexpected tag: {s}", .{tag});
-        }
-    }
-
-    std.debug.assert(nodes.nodes.items.len > 0);
-    const latest_node = &nodes.nodes.items[nodes.nodes.items.len - 1];
-
-    if (xml2.xmlTextReaderHasAttributes(reader) != 0) {
-        // Successfully moved to the first attribute
-        if (xml2.xmlTextReaderMoveToFirstAttribute(reader) == 1) {
-            // Process each attribute
-            while (true) {
-                // Print attribute name and value
-                const attr_name_ptr = xml2.xmlTextReaderConstName(reader);
-                const attr_name = if (attr_name_ptr) |ptr| std.mem.span(ptr) else unreachable;
-                const attr_value_ptr = xml2.xmlTextReaderConstValue(reader);
-                const attr_value = if (attr_value_ptr) |ptr| std.mem.span(ptr) else unreachable;
-
-                const arena_attr_value = try allocator.alloc(u8, attr_value.len);
-                std.mem.copyForwards(u8, arena_attr_value, attr_value);
-
-                switch (latest_node.*) {
-                    inline else => |*n| inline for (std.meta.fields(@TypeOf(n.*))) |field| {
-                        if (std.mem.eql(u8, field.name, attr_name)) {
-                            switch (@typeInfo(field.type)) {
-                                .Int => @field(n, field.name) = try std.fmt.parseInt(u32, arena_attr_value, 10),
-                                else => @field(n, field.name) = arena_attr_value,
-                            }
-                        }
-                    },
-                }
-
-                // Attempt to move to the next attribute; break if unsuccessful
-                if (xml2.xmlTextReaderMoveToNextAttribute(reader) != 1) {
-                    break;
-                }
-            }
-
-            // After processing attributes, move back to the element node
-            _ = xml2.xmlTextReaderMoveToElement(reader);
-        }
-    }
-
-    // if (xml2.xmlTextReaderHasValue(reader) != 0) {
-    //     const value_ptr = xml2.xmlTextReaderConstValue(reader);
-    //     const value = if (value_ptr) |ptr| std.mem.span(ptr) else "";
-    //     // std.debug.print("Value: {s}", .{value});
-    // }
-}
