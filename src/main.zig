b8f75e57af021b8f9ef547ed33a638a3e573786d
Malcolm Still <malcolm.still@gmail.com>
2024-03-01 15:40:47 +0000

    Before properly typing arg

--- src/main.zig
+++ src/main.zig
@@ -26,6 +26,157 @@ pub fn main() !void {
     std.debug.print("{any}", .{nodes});
 }
 
+const NodeList = struct {
+    nodes: std.ArrayList(Node),
+
+    fn init(allocator: std.mem.Allocator) NodeList {
+        return .{ .nodes = std.ArrayList(Node).init(allocator) };
+    }
+
+    fn deinit(node_list: *NodeList) void {
+        node_list.nodes.deinit();
+    }
+
+    fn append(node_list: *NodeList, node: Node) !void {
+        try node_list.nodes.append(node);
+    }
+
+    pub fn format(node_list: NodeList, comptime _: []const u8, _: std.fmt.FormatOptions, writer: anytype) !void {
+        var indent: i32 = -1;
+        for (node_list.nodes.items) |node| {
+            if (isOpen(node)) indent += 2;
+            defer {
+                if (!isOpen(node)) indent -= 2;
+            }
+
+            for (0..@intCast(indent)) |_| {
+                try writer.print(" ", .{});
+            }
+
+            try writer.print("{any}\n", .{node});
+        }
+    }
+};
+
+const prelude =
+    \\const std = @import("std");
+    \\const builtin = @import("builtin");
+    \\const WireFn = @import("wire.zig").Wire;
+;
+
+fn isOpen(node: Node) bool {
+    return switch (node) {
+        .protocol_begin,
+        .copyright_begin,
+        .interface_begin,
+        .description_begin,
+        .event_begin,
+        .request_begin,
+        .enum_begin,
+        .arg_begin,
+        .entry_begin,
+        => true,
+        .protocol_end,
+        .copyright_end,
+        .interface_end,
+        .description_end,
+        .event_end,
+        .request_end,
+        .enum_end,
+        .arg_end,
+        .entry_end,
+        => false,
+    };
+}
+
+const walyand_struct = "const fn Wayland(comptime ResourceMap: struct {{ {s} }}) type {{ return struct {{ {s} }}; }}";
+
+const NodeEnum = enum(u8) {
+    protocol_begin,
+    protocol_end,
+    copyright_begin,
+    copyright_end,
+    interface_begin,
+    interface_end,
+    description_begin,
+    description_end,
+    event_begin,
+    event_end,
+    request_begin,
+    request_end,
+    enum_begin,
+    enum_end,
+    arg_begin,
+    entry_begin,
+    arg_end,
+    entry_end,
+};
+
+const Node = union(NodeEnum) {
+    protocol_begin: struct {
+        name: []const u8 = "",
+    },
+    protocol_end: struct {},
+    copyright_begin: struct {},
+    copyright_end: struct {},
+    interface_begin: struct {
+        name: []const u8 = "",
+        version: u32 = 0,
+    },
+    interface_end: struct {},
+    description_begin: struct { summary: []const u8 = "" },
+    description_end: struct {},
+    event_begin: struct { name: []const u8 = "", since: u32 = 0 },
+    event_end: struct {},
+    request_begin: struct { name: []const u8 = "", since: u32 = 0 },
+    request_end: struct {},
+    enum_begin: struct { name: []const u8 = "", bitfield: bool = false },
+    enum_end: struct {},
+    arg_begin: struct {
+        name: []const u8 = "",
+        type: []const u8 = "",
+        summary: []const u8 = "",
+        @"enum": ?[]const u8 = null,
+        interface: ?[]const u8 = null,
+    },
+    entry_begin: struct {
+        name: []const u8 = "",
+        value: u32 = 0,
+        summary: []const u8 = "",
+    },
+    arg_end: struct {},
+    entry_end: struct {},
+
+    pub fn format(node: Node, comptime _: []const u8, _: std.fmt.FormatOptions, writer: anytype) !void {
+        switch (node) {
+            .protocol_begin => |n| try writer.print("<protocol name=\"{s}\">", .{n.name}),
+            .protocol_end => |_| try writer.print("</protocol>", .{}),
+            .copyright_begin => |_| try writer.print("<copyright>", .{}),
+            .copyright_end => |_| try writer.print("</copyright>", .{}),
+            .interface_begin => |n| try writer.print("<interface name=\"{s}\" version={}>", .{ n.name, n.version }),
+            .interface_end => |_| try writer.print("</interface>", .{}),
+            .description_begin => |n| try writer.print("<description summary=\"{s}\">", .{n.summary}),
+            .description_end => |_| try writer.print("</description>", .{}),
+            .event_begin => |n| try writer.print("<event name=\"{s}\" since={}>", .{ n.name, n.since }),
+            .event_end => |_| try writer.print("</event>", .{}),
+            .request_begin => |n| try writer.print("<request name=\"{s}\" since={}>", .{ n.name, n.since }),
+            .request_end => |_| try writer.print("</request>", .{}),
+            .enum_begin => |n| try writer.print("<enum name=\"{s}\" bitfield={}>", .{ n.name, n.bitfield }),
+            .enum_end => |_| try writer.print("</enum>", .{}),
+            .arg_begin => |n| try writer.print("<arg name=\"{s}\" type=\"{s}\" summary=\"{s}\" enum=\"{?s}\" interface=\"{?s}\">", .{
+                n.name,
+                n.type,
+                n.summary,
+                n.@"enum",
+                n.interface,
+            }),
+            .arg_end => |_| try writer.print("</arg>", .{}),
+            .entry_begin => |n| try writer.print("<entry name=\"{s}\" value={} summary=\"{s}\">", .{ n.name, n.value, n.summary }),
+            .entry_end => |_| try writer.print("</entry>", .{}),
+        }
+    }
+};
+
 fn processNode(allocator: std.mem.Allocator, node_list: *NodeList, maybe_parent_node: ?*xml2.xmlNode) !void {
     var maybe_cur_node: ?*xml2.xmlNode = maybe_parent_node;
 
@@ -67,7 +218,6 @@ fn processNode(allocator: std.mem.Allocator, node_list: *NodeList, maybe_parent_
         while (attr != null) {
             const attr_name = std.mem.span(attr.*.name);
             const attr_value = std.mem.span(attr.*.children.*.content);
-            // std.debug.print("  Attribute: {s}={s}\n", .{ attrName, attrValue });
 
             const arena_attr_value = try allocator.alloc(u8, attr_value.len);
             std.mem.copyForwards(u8, arena_attr_value, attr_value);
@@ -76,7 +226,11 @@ fn processNode(allocator: std.mem.Allocator, node_list: *NodeList, maybe_parent_
                 inline else => |*n| inline for (std.meta.fields(@TypeOf(n.*))) |field| {
                     if (std.mem.eql(u8, field.name, attr_name)) {
                         switch (@typeInfo(field.type)) {
-                            .Int => @field(n, field.name) = try std.fmt.parseInt(u32, arena_attr_value, 10),
+                            .Int => @field(n, field.name) = if (std.mem.startsWith(u8, arena_attr_value, "0x"))
+                                try std.fmt.parseInt(u32, arena_attr_value[2..], 16)
+                            else
+                                try std.fmt.parseInt(u32, arena_attr_value, 10),
+                            .Bool => @field(n, field.name) = std.mem.eql(u8, arena_attr_value, "true"),
                             else => @field(n, field.name) = arena_attr_value,
                         }
                     }
@@ -111,143 +265,3 @@ fn processNode(allocator: std.mem.Allocator, node_list: *NodeList, maybe_parent_
         }
     }
 }
-
-const NodeList = struct {
-    nodes: std.ArrayList(Node),
-
-    fn init(allocator: std.mem.Allocator) NodeList {
-        return .{ .nodes = std.ArrayList(Node).init(allocator) };
-    }
-
-    fn deinit(node_list: *NodeList) void {
-        node_list.nodes.deinit();
-    }
-
-    fn append(node_list: *NodeList, node: Node) !void {
-        try node_list.nodes.append(node);
-    }
-
-    pub fn format(node_list: NodeList, comptime _: []const u8, _: std.fmt.FormatOptions, writer: anytype) !void {
-        var indent: i32 = -1;
-        for (node_list.nodes.items) |node| {
-            const ind = getIndent(node);
-            if (ind > 0) indent += ind;
-            defer {
-                if (ind < 0) indent += ind;
-            }
-
-            for (0..@intCast(indent)) |_| {
-                try writer.print(" ", .{});
-            }
-
-            try writer.print("{any}\n", .{node});
-        }
-    }
-};
-
-const prelude =
-    \\const std = @import("std");
-    \\const builtin = @import("builtin");
-    \\const WireFn = @import("wire.zig").Wire;
-;
-
-fn getIndent(node: Node) i32 {
-    return switch (node) {
-        .protocol_begin,
-        .copyright_begin,
-        .interface_begin,
-        .description_begin,
-        .event_begin,
-        .request_begin,
-        .enum_begin,
-        .arg_begin,
-        .entry_begin,
-        => 1,
-        .protocol_end,
-        .copyright_end,
-        .interface_end,
-        .description_end,
-        .event_end,
-        .request_end,
-        .enum_end,
-        .arg_end,
-        .entry_end,
-        => -1,
-    };
-}
-
-const walyand_struct = "const fn Wayland(comptime ResourceMap: struct {{ {s} }}) type {{ return struct {{ {s} }}; }}";
-
-const NodeEnum = enum(u8) {
-    protocol_begin,
-    protocol_end,
-    copyright_begin,
-    copyright_end,
-    interface_begin,
-    interface_end,
-    description_begin,
-    description_end,
-    event_begin,
-    event_end,
-    request_begin,
-    request_end,
-    enum_begin,
-    enum_end,
-    arg_begin,
-    entry_begin,
-    arg_end,
-    entry_end,
-};
-
-const Node = union(NodeEnum) {
-    protocol_begin: struct {
-        name: []const u8 = "",
-    },
-    protocol_end: struct {},
-    copyright_begin: struct {},
-    copyright_end: struct {},
-    interface_begin: struct {
-        name: []const u8 = "",
-        version: u32 = 0,
-    },
-    interface_end: struct {},
-    description_begin: struct { summary: []const u8 = "" },
-    description_end: struct {},
-    event_begin: struct { name: []const u8 = "" },
-    event_end: struct {},
-    request_begin: struct { name: []const u8 = "" },
-    request_end: struct {},
-    enum_begin: struct {},
-    enum_end: struct {},
-    arg_begin: struct {
-        name: []const u8 = "",
-        type: []const u8 = "",
-        summary: []const u8 = "",
-    },
-    entry_begin: struct { name: []const u8 = "" },
-    arg_end: struct {},
-    entry_end: struct {},
-
-    pub fn format(node: Node, comptime _: []const u8, _: std.fmt.FormatOptions, writer: anytype) !void {
-        switch (node) {
-            .protocol_begin => |n| try writer.print("<protocol name=\"{s}\">", .{n.name}),
-            .protocol_end => |_| try writer.print("</protocol>", .{}),
-            .copyright_begin => |_| try writer.print("<copyright>", .{}),
-            .copyright_end => |_| try writer.print("</copyright>", .{}),
-            .interface_begin => |n| try writer.print("<interface name=\"{s}\" version={}>", .{ n.name, n.version }),
-            .interface_end => |_| try writer.print("</interface>", .{}),
-            .description_begin => |n| try writer.print("<description summary=\"{s}\" >", .{n.summary}),
-            .description_end => |_| try writer.print("</description>", .{}),
-            .event_begin => |n| try writer.print("<event name=\"{s}\">", .{n.name}),
-            .event_end => |_| try writer.print("</event>", .{}),
-            .request_begin => |n| try writer.print("<request name=\"{s}\">", .{n.name}),
-            .request_end => |_| try writer.print("</request>", .{}),
-            .enum_begin => |_| try writer.print("<enum>", .{}),
-            .enum_end => |_| try writer.print("</enum>", .{}),
-            .arg_begin => |n| try writer.print("<arg name=\"{s}\" type=\"{s}\" summary=\"{s}\">", .{ n.name, n.type, n.summary }),
-            .arg_end => |_| try writer.print("</arg>", .{}),
-            .entry_begin => |n| try writer.print("<entry name=\"{s}\">", .{n.name}),
-            .entry_end => |_| try writer.print("</entry>", .{}),
-        }
-    }
-};
